74e2abadc59ef7a40e9a616333a20185e6e0fcb3
Rasmus Jakobsson <rasmus.jakobsson@sap.com>
2024-11-04 17:46:51 +0100

    added comments

--- constraints-transformer/10_paper_results.py
+++ constraints-transformer/10_paper_results.py
@@ -18,11 +18,9 @@ models= [
     (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_evf', constraints_to_be_grouped, None, 0.73,'evaluation_sap_sam_2022_test'),
     (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_seperated', None, None,0.73,'evaluation_sap_sam_2022_test'),
     # 100% UNSEEN DATA
-    """
-    (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_evf_unseen', constraints_to_be_grouped, unseen_case_names, 0.73,'evaluation_sap_sam_2022_test'),
-    (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_evf_unseen_065', constraints_to_be_grouped, unseen_case_names, 0.65,'evaluation_sap_sam_2022_test'),
-    (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_seperated_unseen_065', None, unseen_case_names, 0.65,'evaluation_sap_sam_2022_test'),
-    """
+    #(test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_evf_unseen', constraints_to_be_grouped, unseen_case_names, 0.73,'evaluation_sap_sam_2022_test'),
+    #(test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_evf_unseen_065', constraints_to_be_grouped, unseen_case_names, 0.65,'evaluation_sap_sam_2022_test'),
+    #(test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_seperated_unseen_065', None, unseen_case_names, 0.65,'evaluation_sap_sam_2022_test'),
 ]
 
 for model in models:
