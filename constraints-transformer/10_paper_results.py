1ada2f2aa5a62bf1f97875feb50c6ba7b7086709
Rasmus Jakobsson <rasmus.jakobsson@sap.com>
2024-11-04 17:41:01 +0100

    New try, confused for as what unseen is, also last parameter of models

--- constraints-transformer/10_paper_results.py
+++ constraints-transformer/10_paper_results.py
@@ -1,41 +1,38 @@
-import pandas as pd
+from tqdm import tqdm
+from evaluation.utils import evaluate_constraints
 import pickle
 import os
-from glob import glob
+import pandas as pd
 
 model_checkpoint = "checkpoint-12276"
 model_name='google/flan-t5-small'
 dataset='sap_sam_2022/filtered'
 # Define the path to the output directory where .pkl files for test scores are stored
 prediction_output_dir = f'data/evaluation/{dataset}/test/{model_name}_{model_checkpoint}/'
+test_case_names = [i.split('.')[0] for i in os.listdir(prediction_output_dir)]
 
-# Initialize an empty DataFrame to accumulate results from all files
-all_results = []
+path_to_true_constraints='data/sap_sam_2022/filtered/constraints_to_log_labels'
+constraints_to_be_grouped = ['Response', 'Precedence','Succession','Alternate Succession', 'Alternate Precedence','Alternate Response','Choice','Co-Existence']
 
-# Loop through all .pkl files in the directory
-for file_path in glob(os.path.join(prediction_output_dir, '*.pkl')):
-    # Load each .pkl file
-    with open(file_path, 'rb') as f:
-        results = pickle.load(f)
-    
-    print(results)
-    # Convert each result into a DataFrame with the expected structure
-    for constraint_type, predictions in results:
-        for pred in predictions:
-            print(pred)
-            precision, recall, f1 = pred  # Access by position
-            all_results.append({
-                'constraint_type': constraint_type,
-                'precision': precision,
-                'recall': recall,
-                'f1': f1
-            })
+models= [
+    (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_evf', constraints_to_be_grouped, None, 0.73,'evaluation_sap_sam_2022_test'),
+    (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_seperated', None, None,0.73,'evaluation_sap_sam_2022_test'),
+    # 100% UNSEEN DATA
+    """
+    (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_evf_unseen', constraints_to_be_grouped, unseen_case_names, 0.73,'evaluation_sap_sam_2022_test'),
+    (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_evf_unseen_065', constraints_to_be_grouped, unseen_case_names, 0.65,'evaluation_sap_sam_2022_test'),
+    (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_seperated_unseen_065', None, unseen_case_names, 0.65,'evaluation_sap_sam_2022_test'),
+    """
+]
 
-# Convert the accumulated list into a DataFrame
-df = pd.DataFrame(all_results)
-
-# Group by 'constraint_type' and calculate mean precision, recall, and f1, rounded to 2 decimals
-evaluation_scores = df.groupby('constraint_type').mean().reset_index().round(2)
-
-# Display the results
-print(evaluation_scores)
+for model in models:
+    print(model)
+    print('----- ', model[3],' -----')
+    result =  evaluate_constraints(*model[:-1])
+    output_file_name= f'{model[-1]}_{model[3]}.pkl'
+    df = pd.DataFrame(result)
+    df.to_pickle(output_file_name)
+    if model[4]:
+        print(pd.read_pickle(output_file_name)[['precision','recall','f1']].mean().round(2))
+    else:
+        print(pd.read_pickle(output_file_name)[['constraint_type', 'precision', 'recall','f1']].groupby(['constraint_type']).mean().reset_index().round(2))
