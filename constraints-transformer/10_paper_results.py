4617f9cb7d494274a8df0ec3eb3e0df21263946d
Rasmus Jakobsson <rasmus.jakobsson@sap.com>
2024-11-05 12:30:59 +0100

    just to try higher threshold

--- constraints-transformer/10_paper_results.py
+++ constraints-transformer/10_paper_results.py
@@ -16,7 +16,7 @@ constraints_to_be_grouped = ['Response', 'Precedence','Succession','Alternate Su
 
 models= [
     #(test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_evf', constraints_to_be_grouped, None, 0.73),
-    (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_seperated', None, None,0.79),
+    (test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_seperated', None, None,0.73),
     # 100% UNSEEN DATA
     #(test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_evf_unseen', constraints_to_be_grouped, unseen_case_names, 0.73),
     #(test_case_names, path_to_true_constraints, prediction_output_dir, 'xSemAD_evf_unseen_065', constraints_to_be_grouped, unseen_case_names, 0.65),
