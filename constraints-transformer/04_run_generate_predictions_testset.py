16f36d7a16d6f7bcbcc036f1d7e24c51f337a149
Rasmus Jakobsson <rasmus.jakobsson@sap.com>
2024-12-05 10:19:40 +0100

    added functionallity to run kirans model more than once in a row

--- constraints-transformer/04_run_generate_predictions_testset.py
+++ constraints-transformer/04_run_generate_predictions_testset.py
@@ -9,6 +9,7 @@ import pandas as pd
 from evaluation.utils import generate_prediction_list, sort_constraints, filter_prediction_list_for_eval,sort_constraints_for_eval
 from labelparser.label_utils import constraint_splitter
 import torch
+import argparse
 
 #below would be for the fetched from zendo?
 #model_checkpoint =  "checkpoint-127200/checkpoint-42400"#"checkpoint-118800"
@@ -16,7 +17,6 @@ model_checkpoint = "checkpoint-25848"
 model_name='google/flan-t5-small'
 dataset='sap_sam_2022/filtered'
 max_new_tokens=100
-prediction_output_dir = f'data/evaluation/{dataset}/test/{model_name}_{model_checkpoint}/'
 
 #load train cases
 dataset_for_training_dir = f'data/{dataset}/forTraining'#'data/bpmai/forTraining_flan_t5'
@@ -40,55 +40,70 @@ model = model.to(device)
 # 
 relevant_data_dir = f'data/{dataset}/constraints_to_log_labels/'
 
-if not os.path.exists(prediction_output_dir):
-    os.makedirs(prediction_output_dir)
+# Argument parser setup
+parser = argparse.ArgumentParser(description="Process to get predictions of kirans model with with optional settings.")
+parser.add_argument("-n", type=int, help="Specify the number for nr of times processing (e.g., -n4 for 4).")
 
-filesNotFound = 0
-for model_case_name in tqdm(model_case_names, desc='make predictions'):
-    result_list = []
-    try:
-        # Attempt to open the labels file
-        path_to_labels = os.path.join(relevant_data_dir, f'{model_case_name}.LABELS.pkl')
-        with open(path_to_labels, 'rb') as f:
-            labels = list(pickle.load(f))
+args = parser.parse_args()
+# Accessing the value of -n
+if args.n is not None:
+    print(f"Nr of times to run the model: {args.n}")
+    times_to_run = args.n
+else:
+    print("No specific number provided; proceeding with default one time.")
+    times_to_run = 1
 
-        # Attempt to open the constraints file
-        path_to_constraints = os.path.join(relevant_data_dir, f'{model_case_name}.CONSTRAINTS.pkl')
-        with open(path_to_constraints, 'rb') as f:
-            constraints = pickle.load(f)
-        
-        # Process constraints
-        all_constraint_types_in_model = list(set([i.split('[')[0] for i in constraints]))
-        for constraint_type in all_constraint_types_in_model:
-            # Prepare context for prediction generation
-            context = constraint_type +": <event>" + "<event>".join(labels)
-            
-            # Filter and sort true constraints for the current type
-            true_constraints = [c for c in constraints if c.startswith(f"{constraint_type}[")]
-            true_constraints = sort_constraints(true_constraints, remove_duplicates=True)
-            
-            # Generate predictions based on the context
-            predictions = generate_prediction_list(
-                context, tokenizer, model, 30, 
-                max_new_tokens=max_new_tokens, device=device
-            )
-            
-            # Filter and sort predictions for evaluation
-            predictions = filter_prediction_list_for_eval(model_labels=labels, prediction_c_list=predictions)
-            predictions = sort_constraints_for_eval(predictions, remove_duplicates=True)
-            
-            # Append the results for the current constraint type
-            result_list.append((constraint_type, predictions))
+for i in range(times_to_run):
+    prediction_output_dir = f'data/evaluation/{dataset}/test{i+1}/{model_name}_{model_checkpoint}/'
+    if not os.path.exists(prediction_output_dir):
+        os.makedirs(prediction_output_dir)
 
-        # Save predictions to file
-        file_name_path = f'{prediction_output_dir}{model_case_name}.pkl'
-        with open(file_name_path, 'wb') as f:
-            pickle.dump(result_list, f)
+    filesNotFound = 0
+    for model_case_name in tqdm(model_case_names, desc='make predictions'):
+        result_list = []
+        try:
+            # Attempt to open the labels file
+            path_to_labels = os.path.join(relevant_data_dir, f'{model_case_name}.LABELS.pkl')
+            with open(path_to_labels, 'rb') as f:
+                labels = list(pickle.load(f))
 
-    except (FileNotFoundError, IOError) as e:
-        # Handle the error and continue to the next iteration
-        filesNotFound += 1
-        #print(f"Could not process {model_case_name}: {e}")
-        continue
+            # Attempt to open the constraints file
+            path_to_constraints = os.path.join(relevant_data_dir, f'{model_case_name}.CONSTRAINTS.pkl')
+            with open(path_to_constraints, 'rb') as f:
+                constraints = pickle.load(f)
+            
+            # Process constraints
+            all_constraint_types_in_model = list(set([i.split('[')[0] for i in constraints]))
+            for constraint_type in all_constraint_types_in_model:
+                # Prepare context for prediction generation
+                context = constraint_type +": <event>" + "<event>".join(labels)
+                
+                # Filter and sort true constraints for the current type
+                true_constraints = [c for c in constraints if c.startswith(f"{constraint_type}[")]
+                true_constraints = sort_constraints(true_constraints, remove_duplicates=True)
+                
+                # Generate predictions based on the context
+                predictions = generate_prediction_list(
+                    context, tokenizer, model, 30, 
+                    max_new_tokens=max_new_tokens, device=device
+                )
+                
+                # Filter and sort predictions for evaluation
+                predictions = filter_prediction_list_for_eval(model_labels=labels, prediction_c_list=predictions)
+                predictions = sort_constraints_for_eval(predictions, remove_duplicates=True)
+                
+                # Append the results for the current constraint type
+                result_list.append((constraint_type, predictions))
+
+            # Save predictions to file
+            file_name_path = f'{prediction_output_dir}{model_case_name}.pkl'
+            with open(file_name_path, 'wb') as f:
+                pickle.dump(result_list, f)
+
+        except (FileNotFoundError, IOError) as e:
+            # Handle the error and continue to the next iteration
+            filesNotFound += 1
+            #print(f"Could not process {model_case_name}: {e}")
+            continue
 
 print('DONE! And there where ', filesNotFound, ' files not found')
